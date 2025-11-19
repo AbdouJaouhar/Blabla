8dd935a18c9db1228a4f76d4e813f8b60f98eaac
AbdouJaouhar <abdoujaouhar7@gmail.com>
2025-11-19 14:59:57 +0100

    reorg

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+sudo apt-get install lsb-release curl gpg
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install redis
+
+
 sudo apt-get update -y
 sudo apt-get install -y cuda-12-2
 
--- worker/main.py
+++ worker/main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from localllm!")
+    print("Hello from worker!")
 
 
 if __name__ == "__main__":
